2a803d8b480311c30ec9efa544ce4c46d2788c8b
hola <luis,trejos@utp.edu.co>
2022-07-09 11:10:01 -0500

    Added helloWorld

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+import time
+
+helloList = ['H','e','l','l','o',' ','w','o','r','l','d']
+
+runOrNot = True
+
+while runOrNot == True:
+    for i in range(len(helloList)):
+        print(helloList[i])
+        time.sleep(0.25)
+    print(" ")
