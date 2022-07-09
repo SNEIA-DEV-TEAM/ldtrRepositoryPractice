987a6e0adec5860bf41ffa31e66b6a7176e1b9f1
hola <luis,trejos@utp.edu.co>
2022-07-09 12:11:01 -0500

    Mal

--- helloWorld.py
+++ helloWorld.py
@@ -1,14 +1,1 @@
-import time
-
-helloList = ['H','e','l','l','o',' ','w','o','r','l','d']
-
-runOrNot = True
-
-while runOrNot == True:
-    for i in range(len(helloList)):
-        print(helloList[i])
-        time.sleep(0.1)
-    print(" ")  
-
-def estoEsUnaPrueba(a, b):
-    return a + b
+print("Hello!")
